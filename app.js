69a5d0b56153cddd9b51a25f31ad347c3d9523f5
neshdev999 <71468718+neshdev999@users.noreply.github.com>
2021-02-03 15:24:53 -0500

    Add basic template for service menu page
    service menu page title
    main content with service image tiles
    navigation form
    images for tiles

--- app.js
+++ app.js
@@ -37,10 +37,129 @@ function generateSliderTemplate() {
 }
 
 function searchInputControlBox() {
-    return `<div class="inputControlFormContainer"><form><input id="zip" name="zip" type="text" pattern="[0-9]*" placeholder="Enter your zip code"><input type="button" value="Submit"></form></div>`
+    return `<div class="inputControlFormContainer"><form id="homePageForm"><input id="zip" name="zip" type="text" pattern="[0-9]*" placeholder="Enter your zip code"><input type="submit" value="Submit"></form></div>`
 }
 
+/*----Service Menu Page Template----*/
 
+function serviceMenuPage() {
+    generateServiceMenuPageHeader();
+    generateServiceMenuPageMainContent();
+}
+
+function generateServiceMenuPageHeader() {
+    const serviceMenuBaseHeaderTemplate = serviceMenuHeaderContentTemplate();
+    $('.headerWorkMateApp').html(serviceMenuBaseHeaderTemplate);
+}
+
+function serviceMenuHeaderContentTemplate() {
+    return `<div id="serviceMenuPageHeader"><h2>Services<h2><div>`;
+}
+
+function generateServiceMenuPageMainContent() {
+    const serviceMenuBaseMainContentTemplate = serviceMenuMainContentTemplate();
+    $('.mainWorkMateApp').html(serviceMenuBaseMainContentTemplate);
+}
+
+function serviceMenuMainContentTemplate() {
+    const serviceTileImagesTemplate = generateServiceTileImagesTemplate();
+    const formNavigationControl = serviceMenuNavigationInputControlBox();
+    return [serviceTileImagesTemplate, formNavigationControl];
+
+    //return `<h3>Service Menu main content goes here</h3>`;
+}
+
+function generateServiceTileImagesTemplate() {
+    return `
+    <div class="imagteTileParentWrapper">
+        <div class="imageTilesWrapper">
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/security_systems.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Security Systems</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/pest-control.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Pest Control</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/landscaping.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Landscaping</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/cleaningServices.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Cleaning Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/HVAC.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">HVAC</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/roofing.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Roofing Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/plumbing.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Plumbing</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/electrical.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Electrical Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/windowInstallation.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Window Installation</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/fencing_installation.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Fencing Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/garage-services.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Garage Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/Siding.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Siding Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/solar-panel-installation.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Solar Panel Installation</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/carpet_cleaning.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Carpet Cleaning</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/shades_blinds.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Shades & Blinds</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/carpet-installation.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Carpet Installation</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/painting_services.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Painting Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/fireplace_services.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Fireplace Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/duct-cleaning.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Air Duct Services</div>
+            </div>
+            <div class="singleImageTileNameContainer">
+                <div class="imageContainer"><img src="./images/image-tiles/pool-services.jpg" class="serviceMenuTileImage"></div>
+                <div class="imageTileNameContainer">Pool Services</div>
+            </div>
+        </div>
+    </div>`;
+}
+
+function serviceMenuNavigationInputControlBox() {
+    return `<div class="serviceMenuPageNavigationFormContainer"><form id="serviceMenuPageNavigationForm"><input type="submit" value="Go Back to Home Page"/></form></div>`;
+}
 
 /********** RENDER FUNCTION(S) **********/
 
@@ -53,11 +172,11 @@ function preloadSliderImages(slider) {
     }
 }
 
-function changeSliderImages() {
+function changeSliderImages(slide) {
 
     $('#sliderContainer img').attr({
-        src: sliderStore.sliderImages[imageChangeCounter].imageSource,
-        alt: sliderStore.sliderImages[imageChangeCounter].altText
+        src: slide.sliderImages[imageChangeCounter].imageSource,
+        alt: slide.sliderImages[imageChangeCounter].altText
     });
 
     if (imageChangeCounter === 23) {
@@ -70,14 +189,28 @@ function changeSliderImages() {
 function sliderRender() {
     preloadSliderImages(sliderStore);
     setInterval(function() {
-        changeSliderImages();
+        changeSliderImages(sliderStore);
     }, 3000);
 }
 
 
 
 /********** EVENT HANDLER FUNCTIONS **********/
+function handleHomePageFormSubmission() {
+    $('#homePageForm').submit(function(event) {
+        alert("form handler is called");
+        event.preventDefault();
+        provideRoute(routingParamsHolder.currentPage[1]);
+    });
+}
 
+function handleServiceMenuPageNavigationFormSubmission() {
+    $('#serviceMenuPageNavigationForm').submit(function(event) {
+        alert("Going Back to Home page");
+        event.preventDefault();
+        provideRoute(routingParamsHolder.currentPage[0]);
+    });
+}
 
 
 /********** Routing Function **********/
@@ -86,6 +219,10 @@ function provideRoute(checkFlag) {
     if (checkFlag === 'homePage') {
         homePage();
         sliderRender();
+        handleHomePageFormSubmission();
+    } else if (checkFlag === 'servicesMenuPage') {
+        serviceMenuPage();
+        handleServiceMenuPageNavigationFormSubmission();
     }
 }
 
